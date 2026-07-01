bde5985b2799e85a31d18de5f8f1cdfe9b94fb01
Nabila Septi <nabila@example.com>
2026-07-01 20:30:08 +0700

    Telah di singkronisasi dengan home ke mode anak

--- routes/web.php
+++ routes/web.php
@@ -12,4 +12,4 @@ Route::prefix('mode-anak')->name('mode-anak.')->group(function () {
     Route::get('/sholat', [ModeAnakController::class, 'index'])->name('index');
     Route::get('/sholat/{sholat}', [ModeAnakController::class, 'tampilkanGerakan'])->name('gerakan');
 });
-Route::get('/', [HomeController::class, 'index']);
+
